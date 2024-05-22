82870758192e8d8e0961aa8d91a8051cbb748e88
Wesley Maia <72634262+WMaia9@users.noreply.github.com>
2024-05-22 00:02:10 +0000

    update

--- 02-experiment-tracking/02-homework/hpo.py
+++ 02-experiment-tracking/02-homework/hpo.py
@@ -34,13 +34,19 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(params):
+        # Log parameters to MLflow
+        with mlflow.start_run(nested=True):
+            mlflow.log_params(params)
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
 
-        return {'loss': rmse, 'status': STATUS_OK}
+            # Log the RMSE to MLflow
+            mlflow.log_metric("rmse", rmse)
+
+            return {'loss': rmse, 'status': STATUS_OK}
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
@@ -51,12 +57,13 @@ def run_optimization(data_path: str, num_trials: int):
     }
 
     rstate = np.random.default_rng(42)  # for reproducible results
+    trials = Trials()
     fmin(
         fn=objective,
         space=search_space,
         algo=tpe.suggest,
         max_evals=num_trials,
-        trials=Trials(),
+        trials=trials,
         rstate=rstate
     )
 
--- 02-experiment-tracking/02-homework/train.py
+++ 02-experiment-tracking/02-homework/train.py
@@ -26,7 +26,7 @@ def run_train(data_path: str):
     
     mlflow.sklearn.autolog()
     
-    with mlflow.start_run():
+    with mlflow.start_run() as run:
         rf = RandomForestRegressor(max_depth=10, random_state=0)
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
@@ -34,6 +34,8 @@ def run_train(data_path: str):
         rmse = mean_squared_error(y_val, y_pred, squared=False)
         
         mlflow.log_metric("rmse", rmse)
+        
+        print(f"Run ID: {run.info.run_id}")
 
 
 if __name__ == '__main__':
